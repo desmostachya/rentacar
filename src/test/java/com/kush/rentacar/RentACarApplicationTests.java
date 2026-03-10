cf37cad48013563c744e693ca98cb3b2613bf948
Kush <kushraj1204@gmail.com>
2026-03-10 14:45:06 -0500

    Added basic spring boot skeleton

---
+++ src/test/java/com/kush/rentacar/RentACarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kush.rentacar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentACarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
